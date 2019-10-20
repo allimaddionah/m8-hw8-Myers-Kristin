b13920440ae20979c0aa3fea843d3702fb82ed3b
Kristin Myers <kristinmyers@outlook.com>
2019-10-20 17:27:45 -0400

    Add first drop down code

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 // It is always helpful to use comments in your code!
+var myMainNavLinks = document.getElementsByClassName('main-menu-labels1');
+
+function toggleSubMenu()  {
+  console.log(this.nextElementSibling);
+  this.nextElementSibling.classList.toggle('hide');
+}
+
+for (i = 0; i <myMainNavLinks.length; i++)  {
+  myMainNavLinks[i].addEventListener('click', toggleSubMenu);
+}
