acb470a67fe3de6cd960d59aec1c8e8c479328a1
Kristin Myers <kristinmyers@outlook.com>
2019-10-20 17:33:30 -0400

    Add third menu item JS code

--- main.js
+++ main.js
@@ -23,3 +23,15 @@ function toggleSubMenu()  {
 for (i = 0; i <myMainNavLinks.length; i++)  {
   myMainNavLinks[i].addEventListener('click', toggleSubMenu);
 }
+
+
+var myMainNavLinks = document.getElementsByClassName('main-menu-labels3');
+
+function toggleSubMenu()  {
+  console.log(this.nextElementSibling);
+  this.nextElementSibling.classList.toggle('hide');
+}
+
+for (i = 0; i <myMainNavLinks.length; i++)  {
+  myMainNavLinks[i].addEventListener('click', toggleSubMenu);
+}
